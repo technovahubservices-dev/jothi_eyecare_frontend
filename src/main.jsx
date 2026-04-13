0098ff4cb6086cbb903a7e611feb56b41e4ba9cd
technovahubservices-dev <technovahubservices@gmail.com>
2026-04-13 11:46:41 +0530

    url changed test_jothieyecare

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/jothi_eyecare/test">
+    <BrowserRouter basename="/test_jothiEyecare">
       <App />
     </BrowserRouter>
   </React.StrictMode>
